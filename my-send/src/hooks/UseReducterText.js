c5c993ad54527ba46d7c4b4ed21a69e517968bb7
Andressaray <55494875+Andressaray@users.noreply.github.com>
2021-07-20 11:19:56 -0500

    Mostrar de primeras el ultimo texto consultado

--- my-send/src/hooks/UseReducterText.js
+++ my-send/src/hooks/UseReducterText.js
@@ -1,7 +1,7 @@
 export const userTextReducer = (state = [], action) => {
     switch (action.type) {
         case 'add':
-            return [...state, action.payload]
+            return [action.payload, ...state]
         default:
             return ''
     }
